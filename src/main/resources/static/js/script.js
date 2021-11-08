04d72becb2d868de953a15f5cca7703d7bd390c3
Jairo Rios <jairo.rios@hotmail.com>
2021-11-07 22:25:54 -0300

    ATT VP2

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,58 +1,70 @@
-function submeterFormulario() {
-    let titulo = document.getElementById("titulo").value;
-    let autor = document.getElementById("autor").value;
-    let conteudo = document.getElementById("conteudo").value;
+function submeter() {
+    let nome = document.getElementById("nome").value;
+    let fabricante = document.getElementById("fabricante").value;
+    let quantidade = document.getElementById("quantidade").value;
+    let preco = document.getElementById("preco").value;
 
-    let materia = {
-        'titulo': titulo,
-        'autor': autor,
-        'conteudo': conteudo,
+    let controle = {
+        'nome': nome,
+        'fabricante': fabricante,
+        'quantidade': quantidade,
+        'preco': preco,
     };
 
     function reqListener (){
-        let materia = JSON.parse(this.responseText);
-        adicionarMateria(materia);
+        let controle = JSON.parse(this.responseText);
+        adicionarPControle(controle);
     }
 
     let oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
-    oReq.open("POST", "materia", true);
+    oReq.open("POST", "controle", true);
     oReq.setRequestHeader("Content-Type", "application/json");
-    oReq.send(JSON.stringify(materia));
+    oReq.send(JSON.stringify(controle));
 }
 
-function adicionarMateria (materia){
+function adicionarPControle (controle){
     let container = document.createElement("div");
     container.classList.add("container");
 
-    let titulo = document.createElement("div");
-    titulo.classList.add("row");
-    titulo.innerHTML = "<h2>" + materia.titulo + "</h2>";
+    let nome = document.createElement("div");
+    nome.classList.add("row");
+    nome.innerHTML = "<p>" + controle.nome + "</p>";
 
-    let conteudo = document.createElement("div");
-    conteudo.classList.add("row");
+    let fabricante = document.createElement("div");
+    fabricante.classList.add("row");
+    fabricante.innerHTML = "<p>" + fabricante.nome + "</p>";
+
+    let quantidade = document.createElement("div");
+    quantidade.classList.add("row");
+    quantidade.innerHTML = "<p>" + controle.quantidade + "</p>";
+
+    let preco = document.createElement("div");
+    preco.classList.add("row");
     let p = document.createElement("p");
-    p.innerHTML = materia.conteudo;
-    conteudo.appendChild(p);
+    p.innerHTML = controle.preco;
+    preco.appendChild(p);
 
-    container.appendChild(titulo);
-    container.appendChild(conteudo);
-    document.getElementById("materias").appendChild(container);
+    container.appendChild(nome);
+    container.appendChild(quantidade);
+    container.appendChild(fabricante);
+    container.appendChild(preco);
+    document.getElementById("controles").appendChild(container);
 };
 
-function exibirMaterias() {
+function exibirControle() {
     let oReq = new XMLHttpRequest();
     oReq.onload = function(){
-        let listaDeMaterias = JSON.parse(this.responseText);
-        for (let i = 0; i < listaDeMaterias.length; i++){
-            adicionarMateria((listaDeMaterias[i]));
+        let listaDeControles = JSON.parse(this.responseText);
+        for (let i = 0; i < listaDeControles.length; i++){
+            adicionarPControle((listaDeControles[i]));
         }
     };
-    oReq.open("GET", "materia", true);
+    oReq.open("GET", "controle", true);
     oReq.setRequestHeader("Content-Type", "application/json");
     oReq.send();
 };
 
 window.addEventListener("load", function (){
-    exibirMaterias();
+    exibirControle();
 });
